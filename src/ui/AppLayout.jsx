db1341a5bf844003a1d1ffe2397c7c177f8a329b
stamang <stamang.mrbhutan90@gmail.com>
2025-03-05 12:44:46 +0800

    chore: Add global styles and layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router";
+import Header from "./Header";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: red;
+  overflow: scroll;
+`;
+
+const AppLayout = () => {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+};
+
+export default AppLayout;
---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: green;
+
+  grid-row: 1 / -1;
+`;
+
+const Sidebar = () => {
+  return <StyledSidebar>Sidebar</StyledSidebar>;
+};
+
+export default Sidebar;
